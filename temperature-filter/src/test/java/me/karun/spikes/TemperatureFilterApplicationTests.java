43d3b92d0a08b8cea682b2a1f4ecac9447a022c6
Karun Japhet <karun@japhet.in>
2017-04-18 17:43:03 -0400

    Rename Application Test

--- temperature-filter/src/test/java/me/karun/spikes/TemperatureFilterApplicationTests.java
+++ temperature-filter/src/test/java/me/karun/spikes/TemperatureFilterApplicationTests.java
@@ -7,10 +7,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TemperatureProcessorApplicationTests {
+public class TemperatureFilterApplicationTests {
 
   @Test
-  public void contextLoads() {
-  }
-
+  public void contextLoads() {}
 }
+
